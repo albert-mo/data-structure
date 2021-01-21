233ef8c467ff23e00972b101d7b44e8781d763a9
albert <528613910@qq.com>
2021-01-21 14:51:58 +0800

    add 按位查找

--- C2-LinearList/02-static-implementation.cpp
+++ C2-LinearList/02-static-implementation.cpp
@@ -25,7 +25,7 @@ bool ListInsert(SqList &L, int i, int e)
 {
     if (i < 1 || i > L.length + 1) //判断i的范围是否有效
         return false;
-    if (L.lengtg >= MaxSize) //当前存储空间已满，不能插入
+    if (L.length >= MaxSize) //当前存储空间已满，不能插入
         return false;
     for (int j = L.length; j >= i; j--) //将第i个元素及之后的元素后移
     {
@@ -36,7 +36,11 @@ bool ListInsert(SqList &L, int i, int e)
     return true;
 }
 
-/* 删除元素 */
+/* 删除元素 
+最好时间复杂度=O(1)
+平均时间复杂度=O(1/2 * n)
+最坏时间复杂度=O(n)
+*/
 bool ListDelete(SqList &L, int i, int &e)
 {
     if (i < 1 || i > L.length)
@@ -48,10 +52,19 @@ bool ListDelete(SqList &L, int i, int &e)
     return true;
 }
 
+/* 按位查找
+时间复杂度=O(1)
+ */
+int GetElem(SqList L, int i)
+{
+    return L.data[i - 1];
+}
+
 int main()
 {
     SqList L;    //声明一个
     InitList(L); //初始化顺序表
+
     // 此处省略一些代码，插入几个元素
 
     //尝试“违规”打印整个data数组
@@ -59,11 +72,18 @@ int main()
     //     printf("data[%d]=%d\n", i, L.data[i]);
 
     // return 0;
+
+    // 插入元素
+    for (int i = 0; i < 5; i++)
+    {
+        ListInsert(L, 1, i);
+    }
+
     int e = -1;
     if (ListDelete(L, 3, e))
         printf("已删除第三个元素，删除元素值为=%d\n", e);
     else
         printf("位序i不合法，删除失败\n");
 
-    return0;
+    return 0;
 }
